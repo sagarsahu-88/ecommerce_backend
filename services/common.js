f79e9240e73586ca08fe8ddc95d6320e877b8711
SAGAR SAHU <sagarsahurock9@gmail.com>
2023-11-21 21:44:05 +0530

    Final comitment Done

--- services/common.js
+++ services/common.js
@@ -6,7 +6,7 @@ let transporter = nodemailer.createTransport({
   port: 587,
   secure: false, // true for 465, false for other ports
   auth: {
-    user: 'coderdost@gmail.com', // gmail
+    user: 'sagarsahurock9@gmail.com', // gmail
     pass: process.env.MAIL_PASSWORD, // pass
   },
 });
@@ -31,7 +31,7 @@ exports.cookieExtractor = function (req) {
 
 exports.sendMail = async function ({to, subject, text, html}){
     let info = await transporter.sendMail({
-        from: '"E-commerce" <coderdost@gmail.com>', // sender address
+        from: '"E-commerce" <sagarsahurock9@gmail.com>', // sender address
         to,
         subject,
         text,
